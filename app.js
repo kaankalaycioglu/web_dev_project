5bfa686cec43a2ded2b0d4a53b7d7ee13e5d89f1
Kaan Kalaycıoğlu <40381843+kaankalaycioglu@users.noreply.github.com>
2020-10-29 18:56:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var commentRoutes = require("./routes/comments"),
     indexRoutes = require("./routes/index");
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://dedebeyi:Kalaycio21912@yelpcamp.znnve.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://dedebeyi:<password>@yelpcamp.znnve.mongodb.net/<dbname>?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
